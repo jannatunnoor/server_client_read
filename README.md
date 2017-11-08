33ea3cc100cc309274e4b70eef3d665761962c36
Jannatun Noor <m.opogondo@gmail.com>
2017-11-08 12:16:05 +0600

    cahnges readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # server_client_read
+
+request pattern:
+http://127.0.0.1:8051/
+http://127.0.0.1:8051/abc
+
+wsgi_web_application is a simple wsgi application which only return 'index' or 'hello' based on path_info.
+wsgi_server module is used to read and print wsgi.input values given by the client.
+
+** important if anyone design his own client, then he must have to give a file-like object in env['wsgi.input']
+
+#how to run
+python <module_name.py>
+
+
---
+++ wsgi_web_application.py
@@ -0,0 +1,16 @@
+from wsgiref.simple_server import make_server
+
+def application(env, start_response):
+    path = env.get('PATH_INFO')
+    if path == '/':
+        response_body = "Index"
+    else:
+        response_body = "Hello"
+
+    status = "200 OK"
+    response_headers = [("Content-Length", str(len(response_body)))]
+    start_response(status, response_headers)
+    return response_body
+
+httpd = make_server('127.0.0.1', 8051, application)
+httpd.serve_forever()
